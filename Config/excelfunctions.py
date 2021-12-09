fcbe136602f39dd1ceb77c3f406972ed245a670d
Balasubramanya <divya_balasubramanya@condenast.com>
2021-12-09 12:34:39 +0530

    “test”

--- Config/excelfunctions.py
+++ Config/excelfunctions.py
@@ -4,20 +4,23 @@ import openpyxl
 
 def read_data(sheet_name):
     listli = []
-    path = "/Users/dbalasub/PycharmProjects/pythonProject/Locators/DataSheet (2).xlsx"
+    path = "/Users/dbalasub/PycharmProjects/pythonProject/Locators/DataSheet.xlsx"
     workbook = openpyxl.load_workbook(path)
     sheet = workbook[sheet_name]
     rows = sheet.max_row
     cols = sheet.max_column
 
-    for r in range(2, rows+1):
+    for r in range(2, rows + 1):
+        row = []
         for c in range(1, cols+1):
-            val = sheet.cell(r, c).value
-            listli.append(val)
-            # print(val)
-            # listli.append(sheet.cell(r, c).value)
-
+            row.append(sheet.cell(r, c).value)
+        listli.append(row)
+        # print(val)
+        # listli.append(sheet.cell(r, c).value)
     return listli
+
+
+
 
 
 
--- TestCases/test_ask_a_question_module_end_user.py
+++ TestCases/test_ask_a_question_module_end_user.py
@@ -4,7 +4,7 @@ from TestCases.test_base import BaseTest
 from Config.login_cred import LoginCred
 import time
 import pytest
-import Config.excelfunctions
+from Config.excelfunctions import read_data
 
 
 """Passed"""
@@ -12,16 +12,18 @@ import Config.excelfunctions
 
 class TestAskAQuestionModuleEndUser(BaseTest):
 
+
     # def setUp(self):
     #     # to call the setUp() method of base class or super class.
     #     super().setUp()
 
-    @pytest.mark.parametrize("office,summary,description", Config.excelfunctions.read_data("Ask A Question"))
+    @pytest.mark.parametrize('office,summary, description', read_data("Ask A Question"))
     def test_ask_a_question_module_end_user(self, office, summary, description):
     # def test_ask_a_question_module_end_user(self,):
         print("inside")
         self.driver.get("https://supporttest.condenastint.com/sp")
         time.sleep(3)
+
 
         login_cred_obj = LoginCred(self.driver)
         login_cred_obj.login_cred()
